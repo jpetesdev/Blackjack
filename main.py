3272676d285689a587455cb7cf496d3028d95fdb
Jesse Peters <jessepeters@gmail.com>
2024-09-11 21:13:32 -0500

    Slight refactoring of final results

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def card_selector():
         display_card = f"A{selected_suit[0].upper()}"
     return [selected_value, display_card]
 
+
 def check_for_naturals():
     if (player1.hand[0][0] == 10 and player1.hand[1][0] == 11) or (player1.hand[0][1] == 11 and player1.hand[1][0] == 10):
         if (dealer1.hand[0][0] == 10 and dealer1.hand[1][0] == 11) or (dealer1.hand[0][0] == 11 and dealer1.hand[1][0] == 10):
@@ -25,6 +26,8 @@ def check_for_naturals():
     elif (player1.hand[0][0] == 10 and player1.hand[1][0] == 11) or (player1.hand[0][0] == 11 and player1.hand[1][0] == 10):
         print("Player has a Natural!")
         round = False
+    elif ((dealer1.hand[0][0] == 10 and dealer1.hand[1][0] == 11) or (dealer1.hand[0][0] == 11 and dealer1.hand[1][0] == 10)):
+        print("Dealer has a Natural Blackjack!")
     else:
         print("Did not find any Naturals!")
 
@@ -45,8 +48,13 @@ def hit():
             temp_count += card[0]
 
 def display_hands_score(player, dealer):
+    player1.set_count()
+    dealer1.set_count()
+    print("************************************************************")
     print(f"{player.name}'s hand is: {str(player.hand)}. CURRENT SCORE: {str(player._count)}")
     print(f"{dealer.name}'s hand is: {str(dealer.hand[0])}. CURRENT SCORE: {str(dealer.hand[0][0])}")
+    print("************************************************************")
+    sleep(1)
 
 # Returns an array with two elements: First is the player's final score, second is the dealers final score.
 def display_final_score(player, dealer):
@@ -110,6 +118,7 @@ while round:
     else:
         player1.set_count()
         dealer1.set_count()
+        sleep(1)
         display_hands_score(player1, dealer1)
     # Adding functionality to check for Naturals at start of game.
     next_card = input("Would you like to HIT or STAND?: ")
@@ -124,7 +133,7 @@ while round:
                 if 11 in sublist:
                     target = player1.hand.index(sublist)
                     player1.hand[target][0] = 1
-                    #break
+                    break
     #Checking for hand over 21 with no ACE
     if check_for_over_21() == True:
         player1.set_count()
@@ -140,24 +149,42 @@ while round:
 # IF the dealer would get an ACE and it would put them over 17, but not over 21 they must take it and stand.
 
 while dealer_hand:
+    print()
+    print(f"Dealer's hand is: {str(dealer1.hand)}. CURRENT SCORE: {str(dealer1.set_count())}")
+    sleep(1)
     if (dealer1._count < 17) and (player1._count < 22):
+        print("The Dealer's hand is less than 17, so they must take another card.")
         dealer1.set_hand(card_selector())
-        print(dealer1.hand)
-        print(dealer1.set_count())
-        print(dealer1._count)
+        temp_count = 0
+        for card in dealer1.hand:
+            temp_count += card[0]
+    # Checking for Score Over 21 and if there is an Ace acting as an 11
+        if temp_count > 21:
+            for sublist in dealer1.hand:
+                if 11 in sublist:
+                    target = dealer1.hand.index(sublist)
+                    dealer1.hand[target][0] = 1
+        #print(dealer1.hand)
+        dealer1.set_count()
+        #print(dealer1._count)
         sleep(1)
         if dealer1._count > 21:
             print("Dealer has gone BUST!")
+            sleep(1)
             dealer_hand = False
     if dealer1._count >= 17:
-        print(dealer1.hand)
+        print(f"Dealer's new hand is: {str(dealer1.hand)}. CURRENT SCORE: {str(dealer1.set_count())}")
+        sleep(2)
+        #print(dealer1.hand)
         dealer_hand = False
     
 
 display_final_score(player1, dealer1)
                     
 
-        
+# Improvement Notes
 
-#print(player1.hand)
-#print(cards.card_count)
+'''
+1. Make it so that the "displayed card" for Jacks, Queens, and Kings show J, Q, K instead of 10
+2. Make Naturals portion more accuarte
+'''
